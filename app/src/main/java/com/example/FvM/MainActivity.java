400435e744d9a2eb15176f8e161d46d826fe487e
szbani <74052564+szbani@users.noreply.github.com>
2022-08-17 22:37:12 +0200

    0.53
    -anim
    -logo (early)
    -cleaning

--- app/src/main/java/com/example/FvM/MainActivity.java
+++ app/src/main/java/com/example/FvM/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.FvM;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 import com.example.FvM.databinding.ActivityMainBinding;
@@ -22,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+
+        ConstraintLayout constraintLayout = findViewById(R.id.Main_layout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(6000);
+        animationDrawable.start();
 
         //ugyan az
         // setContentView(R.layout.activity_main);
